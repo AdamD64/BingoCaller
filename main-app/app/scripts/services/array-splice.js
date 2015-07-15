8fd338d82567de2f493287729f7a40591d0b9760
Koolaidman64 <adam.dixon@tombola-567.local>
2015-07-15 10:39:39 +0100

    Removed old comments

--- main-app/app/scripts/services/array-splice.js
+++ main-app/app/scripts/services/array-splice.js
@@ -6,12 +6,6 @@
             var numbers = [];
             console.log(numbers);
 
-//            this.createArray30 = function() {
-//                var numbers30 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-//                numbers.push.apply(numbers,numbers30);
-//                console.log(numbers);
-//            };
-
             this.resetArray = function() {
                 this.reset = numbers.splice(0,numbers.length);
                 console.log(numbers);
--- main-app/app/scripts/services/number-generator.js
+++ main-app/app/scripts/services/number-generator.js
@@ -3,22 +3,6 @@
     angular.module('Tombola.BingoCaller.NumberGen')
         .service('NumberGen', function () {
 
-//            var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-//            var numbers30 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-//            numbers.push.apply(numbers,numbers30);
-//            console.log(numbers);
-
-//            var min = 1,
-//                max = 10;
-
-//            function getRandom() {
-//                for (var i = 0; i < 10; i++) {
-//                    var random = (Math.floor(Math.random() * (max - min))) + min;
-//                    console.log(random);
-//                }}
-//
-//            getRandom();
-
             this.shuffle = function(o) {
                 var j, x, i = o.length;
                 while (i) {
